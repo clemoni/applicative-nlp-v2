2d1cd568d98b356fa41280118b6171cbde0a1473
Clement Liscoet <clement.liscoet@gmail.com>
2021-05-23 01:12:26 +0100

    fix: update sw

--- src/public/index.js
+++ src/public/index.js
@@ -31,7 +31,6 @@ btnsubmit.addEventListener("click", (e) => {
     })
     .catch((error) => fhandler.fireAlert(error));
 });
-
 if ("serviceWorker" in navigator) {
   // Use the window load event to keep the page load performant
   window.addEventListener("load", () => {
